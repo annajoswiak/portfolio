5e05fee47556fc40587cd3be774498d7844a22f2
Anna Joswiak <annajoswiak@Anna-Joswiaks-MacBook-Pro.local>
2020-12-02 14:05:20 -0800

    added footer, fixed navbar, added background body

---
+++ script.js
@@ -0,0 +1,15 @@
+function openNav() {
+    document.getElementById("navbar").style.width = "700px";
+}
+
+function closeNav() {
+    document.getElementById("navbar").style.width = "50px";
+}
+
+// function nameExpand() {
+//     document.getElementsByTagName("h1").style.width = "490px";
+// }
+
+// function nameReturn() {
+//     document.getElementsByTagName("h1").style.width = "160px";
+// }
